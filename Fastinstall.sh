9a7f27ec969891a0ba1b5b23c328b353ae0c7ffe
TRNDX <60893857+TRNDX@users.noreply.github.com>
2020-02-10 23:13:11 +0300

    Add files via upload

---
+++ Fastinstall.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cd $HOME/TRNDX
+rm -rf $HOME/.telegram-cli
+install() {
+rm -rf $HOME/.telegram-cli
+sudo chmod +x tg
+chmod +x TRNDX
+chmod +x ts
+./ts
+}
+if [ "$1" = "ins" ]; then
+install
+fi
+chmod +x install.sh
+lua start.lua
